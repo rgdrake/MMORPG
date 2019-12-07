7c4cb638b43bd0acf5e9063ba5f25f99e4b3ae43
Rob Drake <rgdrake@tilde.life>
2019-12-06 22:07:26 -0600

    basic player.js redone

--- src/player.js
+++ src/player.js
@@ -1,35 +1,41 @@
-var MMORPG = MMORPG || {};
-
-MMORPG.Player = {
-    name: "",
-    attributes: {
-        strength: "",
-        intelligence: "",
-        wisdom: "",
-        dexterity: "",
-        constitution: "",
-        charisma: ""
-    },
-    hit_points: 0,
-    spell_points: 0,
-    rollAttribute: function() { //done this way for the bell curve
-        let min = 1;
-        let max = 6;
-        let die1 = Math.floor(Math.random() * (max - min + 1)) + min;
-        let die2 = Math.floor(Math.random() * (max - min + 1)) + min;
-        let die3 = Math.floor(Math.random() * (max - min + 1)) + min;
-        return die1 + die2 + die3;
-    },
-    rollCharacter: function() {
-        //let sheet = document.getElementById('charSheet');
-        this.attributes.strength = "Str: " + this.rollAttribute();
-        this.attributes.intelligence = "Int: " + this.rollAttribute();
-        this.attributes.wisdom = "Wis: " + this.rollAttribute();
-        this.attributes.dexterity = "Dex: " + this.rollAttribute();
-        this.attributes.constitution = "Con: " + this.rollAttribute();
-        this.attributes.charisma = "Cha: " + this.rollAttribute();
-    }
+function Player() {
+    this.name = "";
+    this.attributes = {
+        str: "",    //Strength
+        int: "",    //Intelligence
+        wis: "",    //Wisdom
+        dex: "",    //Dexterity
+        con: "",    //Constitution
+        cha: ""     //Charisma
+    };
+    this.hitpoints = 0;
+    this.spellpoints = 0;
+    this.x = 0;
+    this.y = 0;
 }
 
-MMORPG.Player.rollCharacter();
-console.log(MMORPG.Player.attributes);
+Player.prototype.rollAttribute = function() {
+    let min = 1;
+    let max = 6;
+    //using the below procedure to get a bell curve of attribute distribution
+    let die1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    let die2 = Math.floor(Math.random() * (max - min + 1)) + min;
+    let die3 = Math.floor(Math.random() * (max - min + 1)) + min;
+    return die1 + die2 + die3;
+}
+
+Player.prototype.rollCharacter = function() {
+    this.attributes.str = "Strenght:     " + this.rollAttribute();
+    this.attributes.int = "Intelligence: " + this.rollAttribute();
+    this.attributes.wis = "Wisdom:       " + this.rollAttribute();
+    this.attributes.dex = "Dexterity:    " + this.rollAttribute();
+    this.attributes.con = "Constitution: " + this.rollAttribute();
+    this.attributes.cha = "Charisma:     " + this.rollAttribute();
+}
+
+Player.prototype.constructor = Player;
+
+let player = new Player();
+player.rollCharacter();
+console.log(JSON.stringify(player));
+
