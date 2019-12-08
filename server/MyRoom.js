9dc2f6177e72f1ebc154cbf1f16aa4e5a36406ce
Rob Drake <rgdrake@tilde.life>
2019-12-08 15:54:47 -0600

    real start of server work, using colyseus

---
+++ server/MyRoom.js
@@ -0,0 +1,20 @@
+const colyseus = require('colyseus');
+
+exports.MyRoom = class extends colyseus.Room {
+
+  onCreate (options) {
+  }
+
+  onJoin (client, options) {
+  }
+
+  onMessage (client, message) {
+  }
+
+  onLeave (client, consented) {
+  }
+
+  onDispose() {
+  }
+
+}
