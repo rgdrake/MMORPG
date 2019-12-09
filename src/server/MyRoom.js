8bf244234b21a2de1ffdc887a6573a2487e90f7c
Rob Drake <rgdrake@tilde.life>
2019-12-09 03:46:08 -0600

    very basic skeleton of server done

---
+++ src/server/MyRoom.js
@@ -0,0 +1,24 @@
+const colyseus = require('colyseus');
+
+exports.MyRoom = class extends colyseus.Room {
+
+	onCreate(options) {
+
+	}
+
+	onJoin(client, options) {
+
+	}
+
+	onMessage(client, options) {
+
+	}
+
+	onLeave(client, options) {
+
+	}
+
+	onDispose() {
+
+	}
+}
