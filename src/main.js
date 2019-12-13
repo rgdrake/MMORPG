ca9d6e0ff9c8ab3f9c971367ce621bfabf7b8da2
Rob Drake <rgdrake@tilde.life>
2019-12-12 21:48:40 -0600

    back to separate functions

--- src/main.js
+++ src/main.js
@@ -7,26 +7,37 @@ MMORPG.Game = {
 	scene: new THREE.Scene(),
 	loader: new THREE.FBXLoader(),
 
-	initScene: function () {
+	initLight: function () {
+		this.scene.add(new THREE.AmbientLight("#FFFFFF"))
+	},
+
+	initGround: function () {
+		let groundGeometry = new THREE.PlaneBufferGeometry(100, 100);
+		let dirt = new THREE.TextureLoader().load('assets/pixel-pave.jpg')
+		let earth = new THREE.MeshLambertMaterial({
+			map: dirt
+		})
+
+		let ground = new THREE.Mesh(groundGeometry, earth);
+		ground.rotation.x = -Math.PI / 2;
+		ground.receiveShadow = true;
+
+		this.scene.add(ground);
+	},
+
+	initCamera: function () {
 		this.camera = new THREE.PerspectiveCamera(
 			45, window.innerWidth / window.innerHeight,
 			0.1, 1000);
+	},
 
-
+	initRenderer: function () {
 		this.renderer = new THREE.WebGLRenderer();
 		this.renderer.setClearColor(new THREE.Color(0xFFFFFF));
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	},
 
-
-		let planeGeometry = new THREE.PlaneGeometry(50, 50);
-		let planeMaterial = new THREE.MeshLambertMaterial({ color: 0x0000FF });
-		let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-		plane.rotation.x = -Math.PI / 2;
-		this.scene.add(plane);
-
-		this.scene.add(new THREE.AmbientLight("#FFFFFF"))
-
-		const that = this;
+	buildScene: function (that) {
 		var _obj = 0;
 		this.loader.load('assets/SM_Bld_Base_01.fbx', function (object) {
 			_obj = object;
@@ -35,6 +46,14 @@ MMORPG.Game = {
 			console.error("ERROR!");
 			console.error(e);
 		});
+	},
+
+	initScene: function () {
+		this.initCamera();
+		this.initRenderer();
+		this.initGround();
+		this.initLight();
+		this.buildScene(this);
 
 		this.camera.position.set(-30, 40, 30);
 		this.camera.lookAt(this.scene.position);
@@ -42,6 +61,8 @@ MMORPG.Game = {
 		document.getElementById('game').appendChild(this.renderer.domElement);
 		this.animate();
 	},
+
+
 
 	animate: function () {
 		requestAnimationFrame(MMORPG.Game.animate);
