2deb271446ab155ffb2d3799b7d39b71a8dab397
Rob Drake <rgdrake@tilde.life>
2020-06-06 00:14:24 -0500

    keycodes done

--- src/keyboard/keyboard.js
+++ src/keyboard/keyboard.js
@@ -18,4 +18,52 @@ class Keyboard {
 		if (this.moveRight)
 			this.object.translateX(this.moveSpeed);
 	}
+
+	onKeyDown(event) {
+		switch (event.keyCode) {
+			case 38: //Up
+			case 87: //W
+				this.moveForward = true;
+				break;
+
+			case 37: //Left
+			case 65: //A
+				this.moveLeft = true;
+				break;
+
+			case 40: //Down
+			case 83: //A
+				this.moveBackward = true;
+				break;
+
+			case 39: //Right
+			case 68: //D
+				this.moveRight = true;
+				break;
+		}
+	}
+
+	onKeyUp(event) {
+		switch (event.keyCode) {
+			case 38: //Up
+			case 87: //W
+				this.moveForward = false;
+				break;
+
+			case 37: //Left
+			case 65: //A
+				this.moveLeft = false;
+				break;
+
+			case 40: //Down
+			case 83: //A
+				this.moveBackward = false;
+				break;
+
+			case 39: //Right
+			case 68: //D
+				this.moveRight = false;
+				break;
+		}
+	}
 }
