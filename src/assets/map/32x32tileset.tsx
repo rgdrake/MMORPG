35529a2049567cc132e6ca3e41033562b6cecbce
Rob Drake <rgdrake@tilde.life>
2020-06-09 03:20:31 -0500

    original map got corrupted, made new map

---
+++ src/assets/map/32x32tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="32x32tileset" tilewidth="32" tileheight="32" tilecount="667" columns="23">
+ <image source="32x32tileset.png" width="736" height="928"/>
+</tileset>
